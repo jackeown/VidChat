d5d20701c7c1400fb903e2491bffb613ebfffd7c
Jack McKeown <jackamckeown@gmail.com>
2021-08-08 22:39:21 -0500

    added turn server

--- frontHelpers.js
+++ frontHelpers.js
@@ -419,33 +419,7 @@ async function createPeerConnection(targetId) {
 
     let iceServers = [
         "stun:stun.l.google.com:19302",
-
-        "stun:iphone-stun.strato-iphone.de:3478",
-        "stun:numb.viagenie.ca:3478",
-        "stun:s1.taraba.net:3478",
-        "stun:s2.taraba.net:3478",
-        "stun:stun.12connect.com:3478",
-        "stun:stun.12voip.com:3478",
-        "stun:stun.1und1.de:3478",
-        "stun:stun.2talk.co.nz:3478",
-        "stun:stun.2talk.com:3478",
-        "stun:stun.3clogic.com:3478",
-        "stun:stun.3cx.com:3478",
-        "stun:stun.a-mm.tv:3478",
-        "stun:stun.aa.net.uk:3478",
-        "stun:stun.acrobits.cz:3478",
-        "stun:stun.actionvoip.com:3478",
-        "stun:stun.advfn.com:3478",
-        "stun:stun.aeta-audio.com:3478",
-        "stun:stun.aeta.com:3478",
-        "stun:stun.alltel.com.au:3478",
-        "stun:stun.altar.com.pl:3478",
-        "stun:stun.annatel.net:3478",
-        "stun:stun.antisip.com:3478",
-        "stun:stun.arbuz.ru:3478",
-        "stun:stun.avigora.com:3478",
-        "stun:stun.avigora.fr:3478",
-        "stun:stun.awa-shima.com:3478"
+        "turn:turn.vidchat.mckeown.in:3478"
     ]
     let conn = new RTCPeerConnection({ iceServers: iceServers.map(s => ({urls: s}))});
 
